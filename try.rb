652533e855596ec2a83374ad3bd5beab7b1c0bbb
Tegan Sims <tegansims@hotmail.com>
2019-06-18 17:27:47 +0000

    Automatically backed up by Learn

--- try.rb
+++ try.rb
@@ -10,3 +10,13 @@ end
 array1 = [2, 4, 6, 8]
 
 find_element_index(array1, 4)
+
+def find_element_index_try2(array, value_to_find)
+  # Add your solution here
+  counter = 0 
+  while counter < array.length
+    array[counter] == value
+    counter +=1
+  end
+  p counter
+end
