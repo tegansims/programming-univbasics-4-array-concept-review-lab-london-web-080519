9623f3ac7f8b4076359a721dd21f038f66fceb8a
Tegan Sims <tegansims@hotmail.com>
2019-06-18 17:45:03 +0000

    Automatically backed up by Learn

--- try.rb
+++ try.rb
@@ -1,24 +1,16 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
-  counter = 0 
-  until array[counter] == value_to_find
-    counter +=1
+  if array.include?(value_to_find)
+      counter = 0 
+    until array[counter] == value_to_find
+      counter +=1
+    end
+    p counter
+  else
+    false
   end
-  p counter
 end
 
 array1 = [2, 4, 6, 8]
 
-
-
-def find_element_index_try2(array, value_to_find)
-  # Add your solution here
-  counter = 0 
-  while counter < array.length
-    if array[counter] == value_to_find
-      puts array[counter]
-    counter +=1
-    end
-  end
-end
-find_element_index_try2(array1, 4)
+find_element_index(array1, 4)
