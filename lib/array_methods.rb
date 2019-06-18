076fee1cdaa20ce295460287b57294eb10f58909
Tegan Sims <tegansims@hotmail.com>
2019-06-18 17:12:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,8 +4,12 @@ end
 
 def find_max_value(array)
   # Add your solution here
+  array.sort! 
+  array[-1]
 end
 
 def find_min_value(array)
   # Add your solution here
+  array.sort! 
+  array[0]
 end
