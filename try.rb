e8b927479026cf57f6bfd6a1ec7f1d2abe176080
Tegan Sims <tegansims@hotmail.com>
2019-06-18 17:29:27 +0000

    Automatically backed up by Learn

--- try.rb
+++ try.rb
@@ -15,7 +15,8 @@ def find_element_index_try2(array, value_to_find)
   # Add your solution here
   counter = 0 
   while counter < array.length
-    array[counter] == value
+    if array[counter] == value
+      
     counter +=1
   end
   p counter
