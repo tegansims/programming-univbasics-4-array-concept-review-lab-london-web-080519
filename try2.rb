b8c0670722c4f09a1692f4781d2b54473f822501
Tegan Sims <tegansims@hotmail.com>
2019-06-18 17:35:41 +0000

    Automatically backed up by Learn

---
+++ try2.rb
@@ -0,0 +1,24 @@
+def find_element_index(array, value_to_find)
+  # Add your solution here
+  counter = 0 
+  until array[counter] == value_to_find
+    counter +=1
+  end
+  p counter
+end
+
+array1 = [2, 4, 6, 8]
+
+
+
+def find_element_index_try2(array, value_to_find)
+  # Add your solution here
+  counter = 0 
+  while counter < array.length
+    if array[counter] == value_to_find
+      puts array[counter]
+    counter +=1
+    end
+  end
+end
+find_element_index_try2(array1, 4)
