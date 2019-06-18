0f89307929bb724ce09627b0f1b212c6a788239f
Tegan Sims <tegansims@hotmail.com>
2019-06-18 17:38:51 +0000

    Automatically backed up by Learn

--- try2.rb
+++ try2.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
   counter = 0 
-  while array.length > counter
+  while counter < array.length
     until array[counter] == value_to_find
       counter +=1
     end
